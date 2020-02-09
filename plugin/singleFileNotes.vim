5960de8ebe1a5ea67eb8494c5a0c0ba6a78eb68e
Boelensman1 <me@wiggerboelens.com>
2020-02-09 12:06:59 +0100

    Implementation

--- plugin/singleFileNotes.vim
+++ plugin/singleFileNotes.vim
@@ -1,6 +1,15 @@
+if !has('python3')
+  echo "Error: Required vim compiled with +python3"
+  finish
+endif
+
+if exists('g:singleFileNotes_plugin_loaded')
+    finish
+endif
+
 let s:plugin_root_dir = fnamemodify(resolve(expand('<sfile>:p')), ':h')
 
-python << EOF
+python3 << EOF
 import sys
 from os.path import normpath, join
 import vim
@@ -9,3 +18,31 @@ python_root_dir = normpath(join(plugin_root_dir, '..', 'python'))
 sys.path.insert(0, python_root_dir)
 import main
 EOF
+
+
+function! AddNote()
+    python3 main.add_note()
+endfunction
+command! -nargs=0 AddNote call AddNote()
+
+function! AddTodo()
+    python3 main.add_todo()
+endfunction
+command! -nargs=0 AddTodo call AddTodo()
+
+function! AddTodoTomorrow()
+    python3 main.add_todo_tomorrow()
+endfunction
+command! -nargs=0 AddTodoTomorrow call AddTodoTomorrow()
+
+" Add mappings that iterm uses 
+:map ,,addnote <Esc>:call AddNote()<CR>
+:imap ,,addnote <Esc>:call AddNote()<CR>
+
+:map ,,addtodo <Esc>:call AddTodo()<CR>
+:imap ,,addtodo <Esc>:call AddTodo()<CR>
+
+:map ,,addtodotomorrow <Esc>:call AddTodoTomorrow()<CR>
+:imap ,,addtodotomorrow <Esc>:call AddTodoTomorrow()<CR>
+
+let g:singleFileNotes_plugin_loaded = 1
--- python/main.py
+++ python/main.py
@@ -1,1 +1,52 @@
-print "Hello from Python source code!"
+import vim
+import datetime
+import math
+
+longestDate = 27 # len(Wednesday 10 September 2020) = 27
+headerlength = longestDate + 4 * 2 + 2 # 2 spaces
+
+def pad(input):
+    padding_length = (27 - len(input)) / 2
+    padding_left = ' ' * (math.floor(padding_length) + 1)
+    padding_right = ' ' * (math.ceil(padding_length) + 1)
+    return padding_left + input + padding_right
+
+def get_datestring(tomorrow=False):
+    date = datetime.date.today()
+    if tomorrow:
+        date = date + datetime.timedelta(days=1)
+    return '=' * 4 + pad(date.strftime("%A %d %B %Y")) + '=' * 4
+
+def add_date(datestring):
+    vim.current.buffer.append(['','=' * headerlength, datestring, '=' * headerlength,'@TODO',''])
+
+def search(pattern):
+    result = vim.eval('search("'+ pattern+'", "c")')
+    return result
+
+def scroll_to_bottom():
+    length = len(vim.current.buffer)
+    vim.current.window.cursor = (length,0)
+
+def add_entry(tomorrow=False):
+    datestring = get_datestring(tomorrow)
+
+    # check if we already have $currentday
+    if int(search(datestring)) is 0:
+        scroll_to_bottom()
+        add_date(datestring)
+
+def add_note():
+    add_entry()
+    # scroll to @TODO
+    search('@TODO')
+    # go up 1 line
+    vim.current.window.cursor = (vim.current.window.cursor[0]-1,0)
+
+def add_todo():
+    add_entry()
+    scroll_to_bottom()
+
+def add_todo_tomorrow():
+    add_entry(True)
+    scroll_to_bottom()
