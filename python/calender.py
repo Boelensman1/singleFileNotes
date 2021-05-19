c36bf95207f019dec14ffd04b7b1e036f9bccb0c
Wigger Boelens <wigger.boelens@gmail.com>
2021-05-19 22:18:59 +0200

    Another attempt to remove error

--- python/calender.py
+++ python/calender.py
@@ -5,12 +5,9 @@ from datetime import date
 from icalendar import Calendar, Event
 from urllib.request import urlopen
 
-if 'TODO_CALENDER_URL' not in os.environ:
-    sys.exit(0)
-
-url = os.environ['TODO_CALENDER_URL']
 
 def get_events():
+    url = os.environ['TODO_CALENDER_URL']
     g = urlopen(url)
     gcal = Calendar.from_ical(g.read())
 
@@ -31,7 +28,8 @@ def get_events():
         return_value.append("- " + event['summary'].capitalize())
     return return_value
 
-if __name__ == '__main__':
-    events = get_events()
-    for event in events:
-        print(event)
+if 'TODO_CALENDER_URL' in os.environ:
+    if __name__ == '__main__':
+        events = get_events()
+        for event in events:
+            print(event)
